6fe2082efc54c8f7a90ba3e4f2ce877187fe842b
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2025-05-28 17:35:13 +0900

    wip: host::types
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -8,37 +8,42 @@ mod tests {
 
     #[test]
     fn it_works() {
-        // let engine = Engine::default();
-        // let component =
-        //     Component::from_file(&engine, "../target/wasm32-wasip2/debug/wasm_command.wasm")
-        //         .expect("failed to load component");
-        // let mut store = Store::new(&engine, ());
-        // let mut linker = wasmtime::component::Linker::new(&engine);
-        // let instance = linker
-        //     .instantiate(&mut store, &component)
-        //     .expect("failed to instantiate component");
+        use wasmtime::component;
+        use wasmtime::*;
 
-        // let read_request_fn = instance
-        //     .get_typed_func::<(Context, Json), ReadSet>(&mut store, "read_request")
-        //     .expect("failed to get read_request function");
-        // let write_request_fn = instance
-        //     .get_typed_func::<(ViewSet, Json), WriteSet>(&mut store, "write_request")
-        //     .expect("failed to get write_request function");
+        let engine = Engine::default();
+        let component = component::Component::from_file(
+            &engine,
+            "../target/wasm32-wasip2/debug/wasm_command.wasm",
+        )
+        .expect("failed to load component");
+        let mut store = Store::new(&engine, ());
+        let mut linker = component::Linker::new(&engine);
+        let instance = linker
+            .instantiate(&mut store, &component)
+            .expect("failed to instantiate component");
 
-        // let args = serde_json::json!({
-        //     "asset": "rose",
-        //     "threshold": 100,
-        //     "supply_amount": 50,
-        //     "supplier": "alice"
-        // });
-        // let read_request = read_request_fn
-        //     .call(&mut store, (Context::default(), args.to_string()))
-        //     .expect("failed to call read_request function");
-        // dbg!(&read_request);
-        // let view_set;
-        // let write_request = write_request_fn
-        //     .call(&mut store, (view_set, args.to_string()))
-        //     .expect("failed to call write_request function");
-        // dbg!(&write_request);
+        let read_request_fn = instance
+            .get_typed_func::<(Context, Json), ReadSet>(&mut store, "read_request")
+            .expect("failed to get read_request function");
+        let write_request_fn = instance
+            .get_typed_func::<(ViewSet, Json), WriteSet>(&mut store, "write_request")
+            .expect("failed to get write_request function");
+
+        let args = serde_json::json!({
+            "asset": "rose",
+            "threshold": 100,
+            "supply_amount": 50,
+            "supplier": "alice"
+        });
+        let read_request = read_request_fn
+            .call(&mut store, (Context::default(), args.to_string()))
+            .expect("failed to call read_request function");
+        dbg!(&read_request);
+        let view_set;
+        let write_request = write_request_fn
+            .call(&mut store, (view_set, args.to_string()))
+            .expect("failed to call write_request function");
+        dbg!(&write_request);
     }
 }
--- host/src/types.rs
+++ host/src/types.rs
@@ -1,1 +1,72 @@
-pub struct NodeKey;
+pub mod general {
+    use std::collections::BTreeMap;
+
+    pub trait Mode {
+        type AccountAsset;
+    }
+
+    pub struct Tree<T: Mode>(BTreeMap<NodeKey, NodeValue<T>>);
+
+    pub struct FuzzyTree<T: Mode>(BTreeMap<FuzzyNodeKey, NodeValue<T>>);
+
+    pub type KeyElem = &'static str;
+
+    pub enum NodeKey {
+        AccountAsset((KeyElem, KeyElem)),
+    }
+
+    pub enum FuzzyNodeKey {
+        AccountAsset((Option<KeyElem>, Option<KeyElem>)),
+    }
+
+    pub enum NodeValue<T: Mode> {
+        AccountAsset(T::AccountAsset),
+    }
+}
+
+pub mod read {
+    use super::general::*;
+
+    pub struct Read;
+
+    impl Mode for Read {
+        type AccountAsset = AccountAssetR;
+    }
+
+    pub type ReadSet = FuzzyTree<Read>;
+
+    pub struct AccountAssetR;
+}
+
+pub mod view {
+    use super::general::*;
+
+    pub struct View;
+
+    impl Mode for View {
+        type AccountAsset = AccountAssetV;
+    }
+
+    pub type ViewSet = Tree<View>;
+
+    pub struct AccountAssetV {
+        pub balance: u32,
+    }
+}
+
+pub mod write {
+    use super::general::*;
+
+    pub struct Write;
+
+    impl Mode for Write {
+        type AccountAsset = AccountAssetW;
+    }
+
+    pub type WriteSet = Tree<Write>;
+
+    pub enum AccountAssetW {
+        Send(u32),
+        Receive(u32),
+    }
+}
